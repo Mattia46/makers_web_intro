510dad53dbe36e78fd7d0d44a25887b51ddf8834
thisdotrob <thisdotrob@gmail.com>
2015-10-26 16:36:34 +0000

    Ch_07

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello world"
 end
+
+get '/secret' do
+  "Secret message!!!!!!!!"
+end
+
+get '/secret/another_secret' do
+  "A secret secret!"
+end
+
+get '/test' do
+  "nothing"
+end
